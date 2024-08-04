6c1ce597392227af80a5c5289f52a2b5ba0f54b8
Amansaxena6 <aman16nov.as@gmail.com>
2024-08-05 00:47:28 +0530

    added new files

---
+++ HonestInsights/static/js/dashboard.js
@@ -0,0 +1,8 @@
+// Change Button Script start from here 
+const changeButton = document.getElementById('changeButton')
+const imageInput = document.getElementById('imageInput')
+
+changeButton.addEventListener('click', function(){
+    imageInput.click();
+})
+// Change Button Script end here 
